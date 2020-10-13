7b178029c7906d1564477088c4a535ab2854495a
Данила Трофимов <danya.trofimov@gmail.com>
2020-10-13 21:08:10 +0300

    Utf8

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -4,14 +4,14 @@ using namespace std;
 int main() {
     setlocale(LC_ALL, "Russian");
     float a{};
-    std::cout << "Введите длину ребра куба: ";
+    std::cout << "‚ведите длину ребра куба: ";
     std::cin >> a;
     while (a <= 0) {
-        std::cout << "Введите длину ребра куба больше 0: ";
+        std::cout << "‚ведите длину ребра куба больше 0: ";
         std::cin >> a;
     }
-    std::cout << "Площадь боковой поверхности : " << 4 * a * a << std::endl;
-    std::cout << "Объем куба : " << a * a * a << std::endl;
+    std::cout << "Џлощадь боковой поверхности : " << 4 * a * a << std::endl;
+    std::cout << "Ћбъем куба : " << a * a * a << std::endl;
 
         system("pause");
     return 0;
