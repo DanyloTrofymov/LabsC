7d1e0250fc2c0ad74c15fd6df028f4b9bb2ff312
Данила Трофимов <danya.trofimov@gmail.com>
2020-10-21 11:44:11 +0300

    comments

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -6,11 +6,11 @@ using namespace std;
 
 int main() {
     setlocale(LC_ALL, "Russian");
-    float a{};
+    float a; //длина ребра куба
     std::cout << "Введите длину ребра куба: ";
-    std::cin >> a;
-    float s = 4 * a * a;
-    float v = a * a * a;
+    std::cin >> a; 
+    float s = 4 * a * a; //вычисление площади боковой поверхности
+    float v = a * a * a; //вычисление обьема
     std::cout << "Площадь боковой поверхности : " << s << std::endl;
     std::cout << "Объем куба : " << v << std::endl;
 
